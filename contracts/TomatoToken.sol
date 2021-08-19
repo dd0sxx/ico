5a704ff48ff117696dbfab1ab7ff16315ed274a9
dd0sxx <contacttheotelonis@gmail.com>
2021-08-18 20:10:50 -0400

    changed name to tomato token

--- contracts/TomatoToken.sol
+++ contracts/TomatoToken.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract dd0sxxToken is ERC20, Ownable {
+contract TomatoToken is ERC20, Ownable {
 
-    constructor() ERC20("dd0sxx", "D0X") {
+    constructor() ERC20("Tomato", "TMT") {
         _mint(msg.sender, 500000);
     }
 
