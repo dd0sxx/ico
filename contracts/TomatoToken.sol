a9753cbd9ae7fa645d1bb85e4783ed2f5e9f5167
dd0sxx <contacttheotelonis@gmail.com>
2021-08-18 20:29:14 -0400

    added transfer tax

--- contracts/TomatoToken.sol
+++ contracts/TomatoToken.sol
@@ -6,10 +6,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract TomatoToken is ERC20, Ownable {
 
     address payable treasury;
+    uint treasuryBalance;
 
-    constructor(address treasuryAddress) ERC20("Tomato", "TMT") {
-        _mint(msg.sender, 500000);
-        setTreasury(treasuryAddress)
+    constructor(address payable treasuryAddress) ERC20("Tomato", "TMT") {
+        setTreasury(treasuryAddress);
+        _mint(treasury, 50000); //initial 10% to the treasury
     }
 
     function transfer(
@@ -17,10 +18,13 @@ contract TomatoToken is ERC20, Ownable {
         address recipient,
         uint256 amount
     ) public {
-
+        uint tax  = (amount / 100) * 2;
+        uint newAmount = amount - tax;
+        treasuryBalance += tax;
+        _transfer(sender, recipient, newAmount);
     }
 
-    function setTreasury (address treasuryAddress) public {
+    function setTreasury (address payable treasuryAddress) public onlyOwner{
         require (treasuryAddress != address(0), 'treasury cannot be address(0)');
         treasury = treasuryAddress;
     }
