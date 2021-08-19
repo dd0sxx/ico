81a30439313ab6b5f8fa8e6c0c2652fc2abb6dbf
dd0sxx <contacttheotelonis@gmail.com>
2021-08-18 23:22:46 -0400

    added pause and seed phase contribute

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -4,8 +4,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ICO is Ownable {
 
+    mapping (address => uint) balances;
     mapping (address => bool) whitelist;
     uint constant goal = 30000 ether;
+    uint totalFunds;
+    bool paused;
     phases phase;
 
     enum phases {
@@ -14,7 +17,11 @@ contract ICO is Ownable {
         open
     }
 
-    function changePhase () public onlyOwner {
+    constructor () {
+        phase = phases.seed;
+    }
+
+    function changePhase () external onlyOwner {
         if (phase == phases.seed) {
             phase = phases.general;
         } else if (phase == phases.general) {
@@ -24,11 +31,33 @@ contract ICO is Ownable {
         }
     }
 
-    function addToWhitelist (address[] memory privateInvestors) public onlyOwner {
+    function togglePause (bool state) public onlyOwner {
+        paused = state;
+    }
+
+    function addToWhitelist (address[] memory privateInvestors) external onlyOwner {
         uint i;
         for (i = 0; i < privateInvestors.length; i++) {
             whitelist[privateInvestors[i]] = true;
         }
+    }
+
+    function recieve () external payable {
+        require (msg.value > 0.01 ether, "not enough ether");
+
+        if (phase == phases.seed) {
+            require(totalFunds + msg.value <= 15000 ether, "seed phase goal cannot be surpassed");
+            require(whitelist[msg.sender] == true, "address not whitelisted for seed sale");
+            require(balances[msg.sender] + msg.value <= 1500 ether, "amount would be more than 1500 ether contributed");
+            totalFunds += msg.value;
+            balances[msg.sender] += msg.value;
+            if (totalFunds == 15000 ether) {
+                phase = phases.general;
+            }
+        }
+
+        if (phase == phases.general) {}
+        if (phase == phases.open) {}
     }
 
 
