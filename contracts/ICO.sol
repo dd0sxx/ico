b5c453c94fdb1980415250930fb705a82cdc3f90
dd0sxx <contacttheotelonis@gmail.com>
2021-08-18 22:43:16 -0400

    created enum for phase and phase fast forward function

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -1,5 +1,28 @@
 pragma solidity ^0.8.0;
 
-contract ICO {
-    
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ICO is Ownable {
+
+    uint constant goal = 30000 ether;
+    phases phase;
+
+    enum phases {
+        seed,
+        general,
+        open
+    }
+
+    function changePhase () public onlyOwner {
+        if (phase == phases.seed) {
+            phase = phases.general;
+        } else if (phase == phases.general) {
+            phase = phases.open;
+        } else {
+            revert("ICO is in phase open");
+        }
+    }
+
+
+
 }
