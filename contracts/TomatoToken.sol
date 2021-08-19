31af402cbeabd7b26fd9e4fefbf4c2a71803204b
dd0sxx <contacttheotelonis@gmail.com>
2021-08-18 20:17:58 -0400

    added treasury variable and function

--- contracts/TomatoToken.sol
+++ contracts/TomatoToken.sol
@@ -5,8 +5,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TomatoToken is ERC20, Ownable {
 
-    constructor() ERC20("Tomato", "TMT") {
+    address payable treasury;
+
+    constructor(address treasuryAddress) ERC20("Tomato", "TMT") {
         _mint(msg.sender, 500000);
+        setTreasury(treasuryAddress)
+    }
+
+    function transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public {
+
+    }
+
+    function setTreasury (address treasuryAddress) public {
+        require (treasuryAddress != address(0), 'treasury cannot be address(0)');
+        treasury = treasuryAddress;
     }
 
 }
