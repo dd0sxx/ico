4c7088292835bfeaeaac6615847055d0fa7bae73
dd0sxx <contacttheotelonis@gmail.com>
2021-08-26 11:17:57 -0400

    creating LP skeleton

---
+++ contracts/LPToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LPToken is ERC20, Ownable {
+
+    address payable public tomatoLP;
+
+}
---
+++ contracts/TomatoLP.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TomatoLP {
+
+    /// @notice will mint LP tokens and deposit liquidity
+   function provideLiquidity () public {
+
+   }
+
+    /// @notice will burn LP tokens and return liquidity
+   function withdrawLiquidity () public {
+
+   }
+
+   /// @notice swap TMTO for ETH
+   function swap () public {
+       // 1% fee
+   }
+
+}
